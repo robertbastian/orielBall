02c593a6ab06389850d512d8d09f0a96e73ea346
robertbastian <robert@bastian.com>
2014-07-03 21:02:08 +0200

    main server https, http server redirects

--- server.js
+++ server.js
@@ -4,37 +4,20 @@ var https = require('https')
 var fs = require('fs')
 var path = require('path')
 var mysql = require('mysql')
-
 var constants = require('./constants')
 
-// HTTPS Server 
-var app = express()
-var port = process.env.port || 3000
-app.set('env','production')
-app.set('port',port)
-https.createServer(constants.certs,app).listen(443,function(){
-  console.log("HTTPS server listening on port "+port)
-})
+var server = express()
 
-// HTTP Server
-var httpApp = express()
-var httpRouter = express.Router()
-httpApp.use('/',httpRouter)
-httpRouter.get('*', function(req,res){
-  res.redirect('https://orielball.uk'+req.url)
-})
-var httpServer = http.createServer(httpApp).listen(80)
-
-app.set('view engine', 'jade')
-app.use(express.static(path.join(__dirname, 'public')))
+server.set('view engine', 'jade')
+server.use(express.static(path.join(__dirname, 'public')))
 
 /*** ROUTES ***/
 
-app.get('/', function(req, res) {
-    res.render('index')
+server.get('/', function(req, res) {
+  res.render('index')
 })
 
-app.get('/subscribe', function(req,res) {
+server.get('/subscribe', function(req,res) {
     var connection = mysql.createConnection(constants.credentials)
     connection.connect()
     connection.query('INSERT INTO mailingList (email,type) VALUES (?,?)', [req.query['email'],req.query['type']],function(err, rows, fields) {
@@ -44,6 +27,13 @@ app.get('/subscribe', function(req,res) {
     connection.end()
 })
 
-app.get('/healthy', function(req,res) {
+server.get('/healthy', function(req,res) {
     res.send('ok')
 })
+
+
+https.createServer(constants.certs,server).listen(443)
+http.createServer(function(req,res){
+  res.writeHead(301,{"Location":"https://orielball.uk/"+req.url})
+  res.end()
+}).listen(80)
