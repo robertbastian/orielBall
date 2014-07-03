0028ca14c4c083065abae8bd9c500429aed376e5
robertbastian <robert@bastian.com>
2014-07-03 17:52:56 +0200

    mysql functionality

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var constants = require('./constants')
+var mysql = require('mysql')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.get('/subscribe', function(req,res) {
-    console.log(req.query)
+    var connection = mysql.createConnection(constants.credentials)
+    connection.connect();
+
+    connection.query('INSERT INTO mailingList (email,type) VALUES (?,?)', req.query[email], req.query[type],function(err, rows, fields) {
+      if (err) res.send('err')
+      else res.send('ok')
+      connection.end();
+    });
+})
+
+router.get('/healthy', function(req,res) {
     res.send('ok')
 })
 
