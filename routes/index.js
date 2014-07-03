c31eff440fa3c2fedf7bba23e46521a78244fe6a
robertbastian <robert@bastian.com>
2014-07-03 16:59:41 +0200

    basic design, front end functionality

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/subscribe', function(req,res) {
+    console.log(req.query)
+    res.send('ok')
+})
+
 module.exports = router;
