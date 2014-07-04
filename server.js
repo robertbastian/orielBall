cb39a9a6a54e75ff4bb94ef31a3ad17bec014e87
robertbastian <robert@bastian.com>
2014-07-04 19:20:19 +0200

    bulk mailing+notifications

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var http = require('http')
 var https = require('https')
 var path = require('path')
 var mysql = require('mysql')
+var fs = require('fs')
+var apn = require('apn')
 var bodyParser = require('body-parser')
+var nodemailer = require('nodemailer')
 var constants = require('./constants')
 var db = mysql.createConnection(constants.mysql)
 
@@ -15,10 +18,14 @@ server.use(express.static(path.join(__dirname, 'public')))
 server.use(bodyParser.json())
 
 
-
 /* Index */
 server.get('/', function(req, res) {
   res.render('index')
+})
+
+server.get('/updates/:number',function(req,res){
+  var message = JSON.parse(fs.readFileSync('updates/'+req.param('number')))
+  res.render('update',message)
 })
 
 /* Health check */
@@ -35,16 +42,85 @@ server.get('/subscribe', function(req,res) {
     )
 })
 
-/* Push subscription */
 
+function sendUpdate(subject, body)
+{
+  var message = {
+    subject: subject,
+    body: body,
+    number: fs.readdirSync('updates/').length + 1
+  }
+
+  fs.writeFile('updates/'+message.number,JSON.stringify(message))
+
+  /* Sending emails */
+  db.query('SELECT email FROM mailingList',
+    function(err,rows,fields)
+    {
+      if (err)
+        console.log('Database error')
+      else
+      {
+        var transport = nodemailer.createTransport("SES",constants.ses)
+        var mailOptions = {
+          from: "Oriel College Ball <marketing@orielball.uk>",
+          subject: message.subject,
+          html: "If the message doesn't display properly, click here: <a href='https://orielball.uk/updates/"+message.number+"'>https://orielball.uk/updates/"+message.number+"</a><br>"+message.body
+        }
+        var recipients = []
+        for (var i = 0, len = rows.length; i < len; i++)
+        {
+          mailOptions['to'] = rows[i]['email']
+          transport.sendMail(mailOptions,function(err,res){ if (err) console.log(err) })
+        }
+        transport.close()
+      }
+    }
+  )
+
+  /* Sending push notification */
+  db.query('SELECT device FROM pushList',
+    function(err,rows,fields)
+    {
+      if (err)
+        console.log('Database error')
+      else
+      {
+        var devices = []
+        for (var i = 0, len = rows.length; i < len; i++)
+          devices.push(new apn.Device(rows[i]['device']))  
+
+        var notification = new apn.Notification()
+        notification.expiry = Math.floor(Date.now()/1000) + 3600 * 24 * 7
+        notification.alert = {
+          "title": "Oriel Ball Update #"+message.number,
+          "body": message.subject,
+          "action":"Read"
+        }
+        notification.urlArgs = [""+message.number] 
+
+
+        var connection = new apn.Connection({
+          "production": true,
+          "key": fs.readFileSync('certificates/ssl-key.pem'),
+          "cert": fs.readFileSync('certificates/push-cert.pem')  
+        })
+        connection.pushNotification(notification,devices)
+        connection.shutdown()
+      }
+    }
+  )
+}
+
+/* Push subscription */
 server.post('/v1/pushPackages/web.uk.orielball',function(req,res){
   res.sendfile('public/pushPackage.zip')
 })
 
 server.post('/v1/devices/:token/registrations/web.uk.orielball',function(req,res){
   db.query(
-    'INSERT INTO pushList (device,type) VALUES (?,?)',
-    [req.param('token'),'oxford'],
+    'INSERT IGNORE INTO pushList (device) VALUE (?)',
+    [req.param('token')],
     function(err, rows, fields) { res.send((err) ? 500 : 200) }
     )
 })
