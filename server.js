e37d99957a254d45af7fb0d5e3d6dcf1c039baa7
Robert Bastian <robert@bastian.com>
2014-08-27 22:07:38 +0000

    restructured views

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var jade = require('jade')
 var http = require('http')
 var https = require ('https')
 var path = require('path')
@@ -8,6 +9,7 @@ var request = require('request')
 var apn = require('apn')
 var bodyParser = require('body-parser')
 var nodemailer = require('nodemailer')
+var mailer = nodemailer.createTransport('direct')
 var constants = require('./constants')
 var stripe = require('stripe')(constants.stripesecret)
 var db = mysql.createConnection(constants.mysql)
@@ -16,33 +18,11 @@ var db = mysql.createConnection(constants.mysql)
 // Creating server
 var server = express()
 // Bodyparser to parse json requests
-server.use(bodyParser.json())
+server.use(bodyParser())
 // Setting jade as view engine
 server.set('view engine', 'jade')
 // Directory for static files
 server.use(express.static(path.join(__dirname, 'public')))
-
-
-// HTTPS server
-https.createServer(constants.ssl,server).listen(443,function(){
-  console.log('Listening for HTTPS requests on port 443')
-})
-
-// HTTP redirect server
-http.createServer(function(req,res){
-  res.writeHeader(301,{'Location':'https://www.orielball.uk'+req.url})
-  res.end()
-}).listen(80,function(){
-  console.log('Redirecting requests at port 80 to https')
-})
-
-// Redirecting orielball.uk, orielball.co.uk, www.orielball.co.uk to www.orielball.uk
-server.all('/*',function(req,res,next){
-  if (req.headers.host != 'www.orielball.uk')
-    res.redirect('https://www.orielball.uk'+req.url)
-  else
-    next()
-})
 
 // Index
 server.get('/', function(req, res){
@@ -87,8 +67,8 @@ server.get('/updates/:number',function(req,res){
 // Tickets
 server.get('/tickets',function(req,res){
   // If before release date
-  if (constants.tickets.date > Date.now() && req.connection.remoteAddress != '93.217.105.83')
-    res.render('ticketsCountdown',{ date: constants.tickets.date })
+  if (constants.tickets.date < Date.now())
+    res.render('tickets/countdown',{ date: constants.tickets.date })
   // Booking live 
   else 
   {
@@ -98,9 +78,9 @@ server.get('/tickets',function(req,res){
         if (err)
           res.send(500,err)
         else if (rows[0]['COUNT(*)'] >= constants.tickets.total)
-          res.render('ticketsSoldOut')
+          res.render('tickets/soldOut')
         else
-          res.render('tickets',{ 
+          res.render('tickets/form',{ 
             left: constants.tickets.total - rows[0]['COUNT(*)'],
             prices: constants.tickets.prices
           })
@@ -120,10 +100,15 @@ server.post('/tickets',function(req,res){
       description: req.body.email,
       statement_description: (req.body.type == 'standard') ? 'Ticket' : 'Dining Ticket'
     },
-    function(err, charge) {
-      if (err && err.type === 'StripeCardError')
-        res.send(200,'Payment error\n'+err)     // The card has been declined
-      if (!err && charge.paid)
+    function(err, charge){
+      if (err || !charge.paid)
+      {
+        if(err.type == 'card_error')
+          res.render('tickets/error',{type: 'payment', message:err.message})
+        else
+          res.render('tickets/error',{type: 'payment', message:'Server error'})
+      }
+      else
       {
         db.query(
           'INSERT INTO bookings (name,email,bodcard,charge,type) VALUES (?,?,?,?,?)',
@@ -131,11 +116,18 @@ server.post('/tickets',function(req,res){
           function(error,rows,fields)
           {
             if (error)
-              res.send('Your payment succeeded but there was a database error. Please contact us at it@orielball.uk with a copy of this page!\n\n\n'+charge+'\n'+error)
+              res.render('tickets/error',{type: 'database', charge: charge.id, error: error})
             else
             {
-              res.send('Success! You should receive a confirmation email shortly')
-              //TODO Send that email!
+              mailer.sendMail(
+                {
+                  from: 'Oriel College Ball <no-reply@orielball.uk>',
+                  to: (req.body.name + '<' + req.body.email + '>'),
+                  subject: 'Ticket confirmation',
+                  html: jade.renderFile('views/tickets/success.jade',{type:req.body.type})
+                }
+              )
+              res.render('tickets/success',{type:req.body.type})
             }
           }
         )
@@ -206,3 +198,25 @@ server.delete('/v1/devices/:token/registrations/web.uk.orielball',function(req,r
 server.post('/v1/log', function(req,res){
   console.log(req.body.logs)
 })
+
+// HTTPS server
+https.createServer(constants.ssl,server).listen(443,function(){
+  console.log('Listening for HTTPS requests on port 443')
+})
+
+// HTTP redirect server
+http.createServer(function(req,res){
+  res.writeHeader(301,{'Location':'https://www.orielball.uk'+req.url})
+  res.end()
+}).listen(80,function(){
+  console.log('Redirecting requests at port 80 to https')
+})
+
+// Redirecting orielball.uk, orielball.co.uk, www.orielball.co.uk to www.orielball.uk
+server.all('/*',function(req,res,next){
+  if (req.headers.host != 'www.orielball.uk')
+    res.redirect('https://www.orielball.uk'+req.url)
+  else
+    next()
+})
+
