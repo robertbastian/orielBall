3094bfe60953ca6f50d173bea4fdb534ba062fc9
Robert Bastian <robert@bastian.com>
2014-08-27 18:30:17 +0000

    Adapted server for elastichost

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var http = require('http')
+var https = require ('https')
 var path = require('path')
 var mysql = require('mysql')
 var fs = require('fs')
@@ -8,28 +9,56 @@ var apn = require('apn')
 var bodyParser = require('body-parser')
 var nodemailer = require('nodemailer')
 var constants = require('./constants')
-var stripe = require("stripe")(constants.stripesecret);
+var stripe = require('stripe')(constants.stripesecret)
 var db = mysql.createConnection(constants.mysql)
 
 
-/* Configuring server */
+// Creating server
 var server = express()
-server.use(bodyParser())
+// Bodyparser to parse json requests
+server.use(bodyParser.json())
+// Setting jade as view engine
 server.set('view engine', 'jade')
+// Directory for static files
 server.use(express.static(path.join(__dirname, 'public')))
 
 
-/* Index */
-server.get('/', function(req, res) {
+// HTTPS server
+https.createServer(constants.ssl,server).listen(443,function(){
+  console.log('Listening for HTTPS requests on port 443')
+})
+
+// HTTP redirect server
+http.createServer(function(req,res){
+  res.writeHeader(301,{'Location':'https://www.orielball.uk'+req.url})
+  res.end()
+}).listen(80,function(){
+  console.log('Redirecting requests at port 80 to https')
+})
+
+// Redirecting orielball.uk, orielball.co.uk, www.orielball.co.uk to www.orielball.uk
+server.all('/*',function(req,res,next){
+  if (req.headers.host != 'www.orielball.uk')
+    res.redirect('https://www.orielball.uk'+req.url)
+  else
+    next()
+})
+
+// Index
+server.get('/', function(req, res){
   db.query(
     'SELECT position, name, email FROM committee ORDER BY id ASC',
     function(err,rows,fields)
     {
-      res.render('index',{committee:(!err)?rows:false,bookingDate:constants.tickets.date})
+      res.render('index',{
+        committee:(!err)?rows:false,
+        bookingDate:constants.tickets.date
+      })
     }
   )
 })
 
+// Draft
 server.get('/topsecret',function(req,res){
   db.query(
     'SELECT position, name, email FROM committee ORDER BY id ASC',
@@ -40,7 +69,7 @@ server.get('/topsecret',function(req,res){
   )
 })
 
-/* Updates */
+// Display subscription email
 server.get('/updates/:number',function(req,res){
   db.query(
     'SELECT text FROM updates WHERE id = ?',
@@ -55,10 +84,12 @@ server.get('/updates/:number',function(req,res){
   )
 })
 
-/* Tickets */
+// Tickets
 server.get('/tickets',function(req,res){
-  if (constants.tickets.date > Date.now())
+  // If before release date
+  if (constants.tickets.date > Date.now() && req.connection.remoteAddress != '93.217.105.83')
     res.render('ticketsCountdown',{ date: constants.tickets.date })
+  // Booking live 
   else 
   {
     db.query(
@@ -78,28 +109,8 @@ server.get('/tickets',function(req,res){
   }
 })
 
-server.get('/ticketsformarkianandcharliebutdontgivethislinktoanyone',function(req,res){
-  if (false)
-    res.render('ticketsCountdown',{ date: constants.tickets.date })
-  else 
-  {
-    db.query(
-      'SELECT COUNT(*) FROM bookings',
-      function(err,rows,fields){
-        if (err)
-          res.send(500,err)
-        else if (rows[0]['COUNT(*)'] >= constants.tickets.total)
-          res.render('ticketsSoldOut')
-        else
-          res.render('tickets',{ 
-            left: constants.tickets.total - rows[0]['COUNT(*)'],
-            prices: constants.tickets.prices
-          })
-      }
-    )
-  }
-})
 
+// Reply from ticket form & payment processing
 server.post('/tickets',function(req,res){
   var charge = stripe.charges.create(
     {
@@ -132,6 +143,8 @@ server.post('/tickets',function(req,res){
   })
 })
 
+
+// Send a string containing the number of remaining tickets (for the counter)
 server.get('/remainingTickets',function(req,res){
   db.query(
     'SELECT COUNT(*) FROM bookings',
@@ -143,12 +156,7 @@ server.get('/remainingTickets',function(req,res){
     })
 })
 
-/* Health check */
-server.get('/check', function(req,res) {
-    res.send(200)
-})
-
-/* Email subscription */
+// Email subscription processing
 server.get('/subscribe', function(req,res) {
   db.query(
     'INSERT INTO mailingList (email,type) VALUES (?,?)',
@@ -157,6 +165,7 @@ server.get('/subscribe', function(req,res) {
     )
 })
 
+// Unsubscribe email processing (not accessible from website)
 server.get('/unsubscribe/:email',function(req,res){
   var email = req.param('email')
   db.query(
@@ -170,11 +179,12 @@ server.get('/unsubscribe/:email',function(req,res){
     })
 })
 
-/* Push subscription */
+// Serving push package
 server.post('/v1/pushPackages/web.uk.orielball',function(req,res){
   res.sendfile('public/pushPackage.zip')
 })
 
+// Registering device
 server.post('/v1/devices/:token/registrations/web.uk.orielball',function(req,res){
   db.query(
     'INSERT IGNORE INTO pushList (device) VALUE (?)',
@@ -183,6 +193,7 @@ server.post('/v1/devices/:token/registrations/web.uk.orielball',function(req,res
     )
 })
 
+// Deleting device
 server.delete('/v1/devices/:token/registrations/web.uk.orielball',function(req,res){
   db.query(
     'DELETE FROM pushList WHERE device = ?',
@@ -191,20 +202,7 @@ server.delete('/v1/devices/:token/registrations/web.uk.orielball',function(req,r
     )
 })
 
+// Push logs
 server.post('/v1/log', function(req,res){
   console.log(req.body.logs)
-})
-
-/* HTTP server */
-http.createServer(server).listen(443,function(){
-  console.log('Listening for HTTP requests on port 443')
-})
-
-/* HTTP redirect server */
-http.createServer(function(req,res){
-  res.writeHead(301,{'Location':'https://www.orielball.uk'+req.url})
-  res.end()
-})
-.listen(80,function(){
-  console.log('Redirecting requests at port 80 to https')
 })
